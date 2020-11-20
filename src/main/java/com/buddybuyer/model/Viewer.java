45691f6977617e274ac34308b5281bae4db6b73c
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2020-11-20 18:47:21 +0700

    Update

--- src/main/java/com/buddybuyer/model/Viewer.java
+++ src/main/java/com/buddybuyer/model/Viewer.java
@@ -1,10 +1,28 @@
 package com.buddybuyer.model;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Viewer {
 	public Viewer(String name) {
 		this.name = name;
 	}
 	String name;
+	Set<Media> favouriteMedia  = new HashSet<>();
+	Set<Genre> favouriteGenres = new HashSet<>();
+	
+	public void likeMedia(Media m) {
+		favouriteMedia.add(m);
+	}
+	public Set<Media> getFavouriteMedia() {
+		return favouriteMedia;
+	}
+	public void likeGenre(Genre g) {
+		favouriteGenres.add(g);
+	}
+	public Set<Genre> getFavouriteGenres() {
+		return favouriteGenres;
+	}
+	
 	public String getName() { 
 		return name; 
 	}
